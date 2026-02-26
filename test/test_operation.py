d6d15d5f9d1ac819c81831ea8896f8b0e6d00851
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-02-25 23:49:19 -0500

    Fix test cases for addition and subtraction

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,11 +1,11 @@
 from src.mathoperation import add, sub
 
 def test_add():
-    assert (4,7) == 11
+    assert (4,8) == 12
     assert (4,2) == 6
     assert (8,3) == 11
 
 def test_sub():
     assert (44,33) == 11
-    assert (78.57) == 21
+    assert (78,57) == 21
     assert (64,7) == 57
