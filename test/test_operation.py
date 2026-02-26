9885e8f9dec0678a51f5aa5a4e2c0822b3853660
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-02-25 23:54:28 -0500

    Fix test cases to use function calls

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,11 +1,11 @@
 from src.mathoperation import add, sub
 
 def test_add():
-    assert (4,8) == 12
-    assert (4,2) == 6
-    assert (8,3) == 11
+    assert add(4,8)==12
+    assert add(4,2)==6
+    assert add(8,3)==11
 
 def test_sub():
-    assert (44,33) == 11
-    assert (78,57) == 21
-    assert (64,7) == 57
+    assert sub(44,33)==11
+    assert sub(78,57)==21
+    assert sub(64,7)==57
